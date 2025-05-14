61835d78693cef00db044f0044ff30cb7655ac1c
MaheswaraKaindra <19623197@std.stei.itb.ac.id>
2025-05-15 00:41:57 +0700

    feat: JavaFX Setup

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,3 +1,5 @@
+package app;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -7,6 +9,7 @@ import javafx.scene.Parent;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        System.out.println("Resource: " + getClass().getResource("/layout.fxml"));
         Parent root = FXMLLoader.load(getClass().getResource("/layout.fxml"));
         Scene scene = new Scene(root);
         stage.setTitle("Rush Hour Solver");
