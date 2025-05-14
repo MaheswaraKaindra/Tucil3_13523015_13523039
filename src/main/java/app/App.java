b650d10588e1cbcc966365f8cfa638bfe0214553
MaheswaraKaindra <19623197@std.stei.itb.ac.id>
2025-05-15 00:08:59 +0700

    chore: Delete Gradle

---
+++ src/main/java/app/App.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/layout.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Rush Hour Solver");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
